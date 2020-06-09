f6d0cc4079ee77a257053a03efab2d17a137614b
eileenlimur <ailin.li@hotmail.com>
2020-06-09 23:18:36 +0000

    added POST method to delete existing tinyURL

--- express_server.js
+++ express_server.js
@@ -23,25 +23,29 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//home
 app.get("/", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
+  let templateVars = { greeting: 'Hey! Let\'s turn long URLs into short URLs!' };
   res.render("hello_world", templateVars);
 });
 
+//show URLS
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
+//shows URLS in JSON
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//page to create new shortURL
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 })
 
+//add new shortURL
 app.post('/urls', (req, res) => {
   const shortUrl = generateRandomString();
   const longUrl = req.body.longURL;
@@ -49,11 +53,21 @@ app.post('/urls', (req, res) => {
   res.redirect('/urls/' + shortUrl);
 });
 
+//small table showing shortURL matched with longURL
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+//deletes shortURL
+app.post('/urls/:shortURL/delete', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL};
+  let urlToDelete = templateVars['shortURL'];
+  delete urlDatabase[urlToDelete];                       
+  res.redirect('/urls');
+});
+
+//if shortURL has longURL, go there, else redirect to shortURLMaker
 app.get('/u/:shortURL', (req, res) => {
   if (!Object.keys(urlDatabase).includes(req.params.shortURL)) {
     res.redirect('/urls/new');
@@ -63,10 +77,16 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longUrl);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+/*
+An unused example of sending an html response directly
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+*/
