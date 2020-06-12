df3a6574d39b65673291f854e25fc6f537f40c8a
eileenlimur <ailin.li@hotmail.com>
2020-06-12 04:32:10 +0000

    modified a screenshot

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ TinyApp is a full stack web application built with Node and Express that allows 
 
 ## Final Product
 
-!["Your very own personal tinyURLs collection"](https://github.com/eileenlimur/tinyapp/blob/master/docs/tinyapp_urls.png)
+!["Your very own personal tinyURLs collection"]()
 !["You can edit your tinyURLs"](https://github.com/eileenlimur/tinyapp/blob/master/docs/tinapp_edit.png)
 
 ## Dependencies
--- databases.js
+++ databases.js
@@ -1,8 +1,8 @@
 const { hashedPassword } = require('./helpers');
 
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "peachesli", dateCreated: '2020-06-12'},
-  "9sm5xK": { longURL: "http://www.google.com", userID: "eileenlimur", dateCreated: '2020-06-12'}
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "peachesli", dateCreated: '2020-6-12'},
+  "9sm5xK": { longURL: "http://www.google.com", userID: "eileenlimur", dateCreated: '2020-6-12'}
 };
 
 const users = {
